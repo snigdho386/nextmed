154676307e1edc648d9706478cf4abe4c833f288
snigdho386 <snigdho386@gmail.com>
2025-06-14 00:46:43 +0530

    init

---
+++ nextmed/src/test/java/com/example/nextmed/NextmedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nextmed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextmedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
